7c1c155d80017e870dcf45da1c6aa8744d054314
Dominic Talbot <Dominictalbot@live.co.uk>
2025-05-30 15:05:32 -0400

    feat: add Radix UI components and utility functions
    - Added new dependencies for Radix UI components including Accordion, Alert Dialog, Dialog, Popover, and others.
    - Implemented Accordion, AlertDialog, Badge, Button, Calendar, Card, Carousel, Dialog, Input, Popover, Skeleton, Sonner, Textarea components with respective styles and functionalities.
    - Created utility function `cn` for merging class names using clsx and tailwind-merge.
    - Introduced a new components.json configuration file for component styling and aliases.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Built with love by Talbot Design Co.</div>
+  return <div>Built with love by Talbot Design Co.
+    <br />
+<Button variant="journal">Button</Button>
+  </div>
 }
